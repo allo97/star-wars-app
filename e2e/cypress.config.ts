564182d48c1ddd56808f3626af3b7cb11a804ae5
Alek <slodczyk97@gmail.com>
2024-09-23 21:54:38 +0200

    added bootstrap, angular material, prettier config

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -8,11 +8,10 @@ export default defineConfig({
       cypressDir: 'src',
       webServerCommands: {
         default: 'npx nx run star-wars-app:serve',
-        production: 'npx nx run star-wars-app:serve-static',
+        production: 'npx nx run star-wars-app:serve-static'
       },
-      ciWebServerCommand: 'npx nx run star-wars-app:serve-static',
-      ciBaseUrl: 'http://localhost:4200',
+      ciWebServerCommand: 'npx nx run star-wars-app:serve-static'
     }),
-    baseUrl: 'http://localhost:4200',
-  },
+    baseUrl: 'http://localhost:4200'
+  }
 });
